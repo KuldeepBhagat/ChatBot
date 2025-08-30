6938689ce07a91d3a7be81648934899d4c4ed17c
Kuldeep <kuldeep496242@gmail.com>
2025-08-30 23:28:28 +0530

    sdiebar opening and closing final setup

--- public/script/main.js
+++ public/script/main.js
@@ -1,10 +1,20 @@
-const sidebar = document.getElementById('sidebar')
-const sidebar_button = document.getElementById('sidebar_button')
-const sidebar_button_mobile = document.getElementById('sidebar_button_mobile')
+const sidebar = document.getElementById('Sidebar');
+const sidebar_button = document.getElementById('Sidebar_button');
+const sidebar_button_mobile = document.getElementById('Sidebar_button_mobile');
+const overaly = document.getElementById('Overlay');
+const main_contet = document.getElementById('Main_content');
 
-sidebar_button.addEventListener('click', () => {
-    sidebar.classList.add('open')
-})
-sidebar_button_mobile.addEventListener('click', () => {
-    sidebar.classList.add('open')
-})
+function Sidebar_functions() {
+    if(sidebar.classList.contains('open')) 
+    {
+        sidebar.classList.remove('open')
+        overaly.classList.remove('active')
+        main_contet.classList.remove('sidebar_active')
+    } else {
+       sidebar.classList.add('open')
+       overaly.classList.add('active')
+       main_contet.classList.add('sidebar_active')
+    }
+}
+sidebar_button.addEventListener('click', () => Sidebar_functions());
+overaly.addEventListener('click', () => Sidebar_functions());
