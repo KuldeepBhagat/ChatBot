079ad389dae2b80b8d5de599bc08f8ff2fd49c8f
Kuldeep <kuldeep496242@gmail.com>
2025-09-03 15:09:25 +0530

    fixed overflowing issues with border-box

--- public/script/main.js
+++ public/script/main.js
@@ -1,9 +1,7 @@
 const sidebar = document.getElementById('Sidebar');
-const sidebar_button = document.getElementById('Sidebar_button');
-const sidebar_button_mobile = document.getElementById('Sidebar_button_mobile');
 const overaly = document.getElementById('Overlay');
 const main_contet = document.getElementById('Main_content');
-const sidebar_button_rotate = document.getElementById('sidebar_open_btn_img')
+const sidebar_button_rotate = Array.from(document.querySelectorAll('.sidebar_open_btn_img'))
 
 function Sidebar_functions() {
     if(sidebar.classList.contains('open')) 
@@ -12,16 +10,22 @@ function Sidebar_functions() {
         sidebar.classList.remove('open')
         overaly.classList.remove('active')
         main_contet.classList.remove('sidebar_active')
-        sidebar_button_rotate.classList.remove('rotate')
+        sidebar_button_rotate.forEach(btn => {
+            btn.classList.remove('rotate')
+        }) 
+        
 
     } else {
 
        sidebar.classList.add('open')
        overaly.classList.add('active')
        main_contet.classList.add('sidebar_active')
-       sidebar_button_rotate.classList.add('rotate')
+       sidebar_button_rotate.forEach(btn => {
+            btn.classList.add('rotate')
+        })
     }
 }
-sidebar_button.addEventListener('click', () => Sidebar_functions());
-sidebar_button_mobile.addEventListener('click', () => Sidebar_functions())
+document.querySelectorAll('.sidebar_button').forEach( btn => {
+    btn.addEventListener('click', () => Sidebar_functions());
+})
 overaly.addEventListener('click', () => Sidebar_functions());
