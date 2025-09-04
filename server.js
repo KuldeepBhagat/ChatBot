d09ae9ca6f9f52894588b6d3fbc81d75a6d0e388
Kuldeep <kuldeep496242@gmail.com>
2025-09-05 00:27:21 +0530

    openrouter chatbot api tested and integreted

--- server.js
+++ server.js
@@ -1,48 +1,20 @@
-import OpenAI from "openai";
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import authRoutes from './routes/auth.js';
+import OpenAI from "openai"
+import "dotenv/config"
+import  express from "express"
 
-dotenv.config();
-const app = express();
-const port = 3000;
-app.use(cors())
-app.use(express.json())
-
-app.use(bodyParser.json())
-
-
-const openai = new OpenAI({
-    baseURL: "https://openrouter.ai/api/v1",
-    apiKey: process.env.CHATBOT_KEY
-})
-                                                     // Bot Response
-app.get('/chatbot', async (req, res) => {
-    try {
-        const query = req.query.q;
-        const result = await openai.chat.completions.create({
-        model: "deepseek/deepseek-r1-0528-qwen3-8b:free",
-        messages: [{role: "user", content: query}],
-    });
-    res.json(result.choices[0].message.content)
-    console.log("data fetched")
-    } catch(err) {
-        const Error = err.name + ': ' + err.message
-        console.error(Error)
-        res.status(500).json({error: Error})
-    }
+const client  = new OpenAI({
+    apiKey: process.env.CHATBOT_KEY,
+    baseURL: "https://openrouter.ai/api/v1",    
 })
 
-                                                         // DataBase
+const response = await client.chat.completions.create({
+    model: "deepseek/deepseek-chat-v3.1:free",
+    messages: [
+        {
+            role: "user",
+            content: "hello how are you",
+         }
+    ]
+})
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDb Connected'))
-    .catch(err => console.error('DB Error: ', err));
-
-app.use('/api', authRoutes);
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+console.log(response.choices[0].message)
