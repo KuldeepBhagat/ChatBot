6822170b5b29947d8829c1e8eb5bc0ca7abb24b0
Kuldeep <kuldeep496242@gmail.com>
2025-09-20 14:50:23 +0530

    mobile view fix test

--- public/script/main.js
+++ public/script/main.js
@@ -141,4 +141,11 @@ user_input.addEventListener('keydown', function(event) {
     }
 })
 
+// Set --vh as 1% of the viewport height
+function setVh() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVh();
+window.addEventListener('resize', setVh);
 
